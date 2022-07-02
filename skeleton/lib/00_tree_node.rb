63d013f7d860a8c1bd400ec59e6f0abf79516545
AA Student <appacademy@Mini-2.local>
2022-07-01 17:17:48 -0700

    Knight

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -58,11 +58,57 @@ class PolyTreeNode
 
     end
 
-
-
-
-
-
-
 end
 
+
+
+class KnightPathFinder
+
+    attr_accessor :start_pos, :considered_pos
+
+    def self.root_node
+        root_node = PolyTreeNode.new(start_pos)
+    end
+
+    def self.valid_moves(pos)
+        row = pos[0]
+        col = pos[1]
+        current_pos[row][col]
+        shift_pos = [[1, 2], [2, 1], [2, -1], [1, -2],[-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+        new_pos = []
+        shift_pos.each do |p|
+            new_p = current_pos + p
+            new_pos << new_p
+        end
+        
+        return new_pos
+    end
+
+    def initialize(start_pos)
+        @start_pos = start_pos
+        @considered_pos = [start_pos]
+    end
+
+    def start_pos
+        @start_pos
+    end
+
+    def new_move_positions(positions)
+        new_pos = []
+        positions.each do |pos|
+        if considered_pos.valid_moves(pos) && !considered_pos.include?(pos)
+            @considered_pos << pos
+            new_pos <<pos
+        end
+
+        return new_pos
+    end
+
+
+
+    
+    
+end
+
+k = KnightPathFinder.new([0,0])
+p k
